4d99c241caaed7a3bca83675efe70e9f53cbf286
Skoblov Aleksandr <smart@blackrabbit.info>
2024-04-09 15:16:57 +0200

    intent

--- intent/script.js
+++ intent/script.js
@@ -2,18 +2,23 @@ const url = 'intent://thesmart19.github.io/intent/meta.html#Intent;scheme=https;
 
 const text = {
     click: 'Click required...',
+    fullscreenAvailable: 'Fullscreen mode is available.',
+    fullscreenNotAvailable: 'Fullscreen mode is not available...',
     redirect: 'Please wait while redirecting...',
 }
 
 if (document.currentScript?.spellcheck) {
-    document.querySelector('.info').innerText = text.redirect
+    document.querySelector('.info').innerText += text.redirect
     document.location.href = url
 } else {
-    document.querySelector('.info').innerText = text.click
+    document.querySelector('.info').innerText += text.click
     document.addEventListener('click', () => {
         document.location.href = url
-        setTimeout(() => {
-            document.location.href = url
-        }, 250)
     })
+}
+
+if (document.fullscreenEnabled) {
+    document.querySelector('.info').innerText += text.fullscreenAvailable
+} else {
+    document.querySelector('.info').innerText += text.fullscreenNotAvailable
 }
