4bd526a4f5ad4665e699380bb89e04e92fdc9aad
Skoblov Aleksandr <smart@blackrabbit.info>
2024-04-09 12:58:36 +0200

    intent

--- intent/script.js
+++ intent/script.js
@@ -1,8 +1,15 @@
 const url = 'intent://thesmart19.github.io/intent/js.html#Intent;scheme=https;package=com.android.chrome;end'
 
+const text = {
+    click: 'Click required...',
+    redirect: 'Please wait while redirecting...',
+}
+
 if (document.currentScript?.spellcheck) {
+    document.querySelector('.info').innerText = text.redirect
     document.location.href = url
 } else {
+    document.querySelector('.info').innerText = text.click
     document.addEventListener('click', () => {
         document.location.href = url
     })
