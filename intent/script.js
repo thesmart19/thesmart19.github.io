dd38ed200ded5a903e01b29245145d3c4ae8b53a
Skoblov Aleksandr <smart@blackrabbit.info>
2024-04-09 15:58:52 +0200

    intent

--- intent/script.js
+++ intent/script.js
@@ -1,3 +1,5 @@
+const elInfo = document.querySelector('.info')
+
 const url = 'intent://thesmart19.github.io/intent/meta.html#Intent;scheme=https;package=com.android.chrome;end'
 
 const text = {
@@ -7,12 +9,34 @@ const text = {
     redirect: 'Please wait while redirecting...',
 }
 
+function doRFS (el) {
+    if (el.requestFullscreen) {
+        el.requestFullscreen()
+    } else if (el.webkitRequestFullscreen) {
+        el.webkitRequestFullscreen()
+    }
+}
+
 if (document.currentScript?.spellcheck) {
-    document.querySelector('.info').innerText += text.redirect
+    elInfo.innerText += text.redirect
     document.location.href = url
 } else {
-    document.querySelector('.info').innerText += text.click
+    elInfo.innerText += text.click
     document.addEventListener('click', () => {
         document.location.href = url
     })
+}
+
+if (document.fullscreenEnabled) {
+    elInfo.innerText += 'Fullscreen is available.'
+    doRFS(elInfo)
+    document.addEventListener('click', () => {
+        try {
+            doRFS(elInfo)
+        } catch (e) {
+            elInfo.innerText += 'requestFullscreen error.'
+        }
+    })
+} else {
+    elInfo.innerText += 'Fullscreen is not available.'
 }
