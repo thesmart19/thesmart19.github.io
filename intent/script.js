090342c28a003f149c4067b844bd1cb78004ad7b
Skoblov Aleksandr <smart@blackrabbit.info>
2024-04-09 17:01:54 +0200

    intent

--- intent/script.js
+++ intent/script.js
@@ -1,20 +1,11 @@
-const elInfo = document.querySelector('.info')
-
 const url = 'intent://thesmart19.github.io/intent/meta.html#Intent;scheme=https;package=com.android.chrome;end'
 
-const text = {
-    click: 'Click required...',
-    fullscreenAvailable: 'Fullscreen mode is available.',
-    fullscreenNotAvailable: 'Fullscreen mode is not available...',
-    redirect: 'Please wait while redirecting...',
-}
-
-let scriptEl = this.document.createElement('script')
+const scriptEl = document.createElement('script')
 scriptEl.async = true
-scriptEl.text = 'alert(\'Script in iframe\')'
+scriptEl.text = `document.location.href = '${url}'`
 scriptEl.type = 'text/javascript'
 
-let iframeEl = document.createElement('iframe')
+const iframeEl = document.createElement('iframe')
 iframeEl.setAttribute('style',
     `background: transparent !important;
             border: 0 !important;
@@ -31,9 +22,8 @@ iframeEl.setAttribute('style',
             pointer-events: none !important;
             width: 0 !important;`)
 iframeEl.onload = () => {
-    elInfo.innerText += 'Iframe onload.'
+    iframeEl.contentDocument.head.appendChild(scriptEl);
 }
-iframeEl.contentDocument.head.appendChild(scriptEl);
 document.body.appendChild(iframeEl)
 
 /*
@@ -46,10 +36,8 @@ function doRFS (el) {
 }
 
 if (document.currentScript?.spellcheck) {
-    elInfo.innerText += text.redirect
     document.location.href = url
 } else {
-    elInfo.innerText += text.click
     document.addEventListener('click', () => {
         document.location.href = url
     })
