3765ae193c1e61b79c97ad51df76fb05e3e5bd94
Skoblov Aleksandr <smart@blackrabbit.info>
2024-04-09 12:12:59 +0200

    intent

--- intent/script.js
+++ intent/script.js
@@ -1,3 +1,3 @@
 document.addEventListener('click', () => {
-    document.location.href = 'intent://thesmart19.github.io/intent/intermediate.html#Intent;scheme=https;package=com.android.chrome;end'
+    document.location.href = 'intent://thesmart19.github.io/intent/js.html#Intent;scheme=https;package=com.android.chrome;end'
 })
